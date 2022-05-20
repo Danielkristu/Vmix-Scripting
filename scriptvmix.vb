aac127d090734fa80dc5374bd49da995404e1d9d
Daniel Kristio Utomo <79684620+Danielkristu@users.noreply.github.com>
2022-05-20 19:14:13 +0700

    Update scriptvmix.vb

--- scriptvmix.vb
+++ scriptvmix.vb
@@ -1,4 +1,13 @@
 API.Function("OverlayInputAllOff")
-API.Function("OverlayInput3In")
+API.Function("OverlayInput3In",Input:=12)
 Sleep(5000)
-API.Function("OverlayInput3Out")
+API.Function("OverlayInput3Out",Input:=12)
+
+
+API.Function("OverlayInput1In",Input:=11)
+Sleep(8000)
+API.Function("OverlayInput4In",Input:=11)
+Sleep(9000)
+API.Function("OverlayInput4Out",Input:=11)
+      
+      
